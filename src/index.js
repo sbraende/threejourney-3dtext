a151cca45a56cd26774c36b062c9c7ff11bd3d1d
sbraende <sebastian.braende@gmail.com>
2024-08-22 14:28:12 +0200

    Modified code to become a fullwindow application.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
 import * as THREE from "three"
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
-import GUI from "lil-gui"
-// import typefont from "three/examples/fonts/helvetiker_regular.typeface.json" // Default font
 import { FontLoader } from "three/addons/loaders/FontLoader.js"
 import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry.js"
 
+// Get html element
 const canvasEl = document.getElementById("canvas")
-const gui = new GUI()
 
+/*
+* Get size for window application 
+*/
 const sizes = {
-    "width": 1024,
-    "height": 1024
+    width: window.innerWidth,
+    height: innerHeight
 }
+
+window.addEventListener("resize", () => {
+    // Get width and height from browser
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera with new aspec ratio
+    camera.aspect = sizes.width/sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+
+    // Fix if user moves window between screens with different aspect ratio
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
 
 
 /* 
@@ -19,47 +36,38 @@ const sizes = {
 */
 const scene = new THREE.Scene()
 
-/* 
-* Material
-*/ 
-const material = new THREE.MeshBasicMaterial( {color: "red"} )
 
 /*
-* Texture loader
+* Textures and Materials
 */
 const textureLoader = new THREE.TextureLoader()
 
 const matcapTexture = textureLoader.load("/textures/matcaps/3.png")
 matcapTexture.colorSpace = THREE.SRGBColorSpace // matcap is incoded in SRGB colourspace 
-
+const masterMaterial = new THREE.MeshMatcapMaterial( {matcap: matcapTexture} )
 
 
 /*
-* Torus
+* Scattered donuts
 */
-console.time("donuts") // used to time how long a section of JS takes
 const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45)
-const donutMaterial = new THREE.MeshMatcapMaterial() // Could use same mat on donut and text
-donutMaterial.matcap = matcapTexture
-donutMaterial.colorSpace = THREE.SRGBColorSpace
 
 for (let i = 0; i<100; i++) {
-    const donut = new THREE.Mesh(donutGeometry, donutMaterial)
+    const donut = new THREE.Mesh(donutGeometry, masterMaterial)
 
     donut.position.x = (Math.random() - 0.5) * 10
     donut.position.y = (Math.random() - 0.5) * 10
     donut.position.z = (Math.random() - 0.5) * 10
 
     donut.rotation.x = Math.random() * Math.PI // use Math.PI*2 if you needed full rotation
-    donut.rotation.y = Math.random() * Math.PI * 2 
+    donut.rotation.y = Math.random() * Math.PI 
 
-    const donusScale = Math.random()
+    const donusScale = Math.random() + 0.02 
     donut.scale.set(donusScale, donusScale, donusScale)
 
     scene.add(donut)
 }
 
-console.timeEnd("donuts") // used to time how long a section of JS takes
 
 /*
 * Fonts
@@ -72,69 +80,44 @@ fontLoader.load(
         const textGeometry = new TextGeometry(
             "Daft Punk",
             { 
-                font: font, // We can use 'font' shorthand instead of writing 'font: font'
+                font, 
                 size: 0.5,
                 depth: 0.2, 
-                curveSegments: 6, // How many segments cicular forms (eg. e)
+                curveSegments: 12,
                 bevelEnabled: true, 
                 bevelThickness: 0.03,
                 bevelSize: 0.02, 
                 bevelOffset: 0, 
-                bevelSegments: 3 // Keep to minimum
+                bevelSegments: 3 
             }
         )
-
-        /*
-        * How to center text with the use of boundingbox
-        */
-
-        // We translate the geometry by half of the max boundingbox to center the text to its pivot. 
-        // We have to correct for bevel size to get exact center
-        // textGeometry.computeBoundingBox()
-        // textGeometry.translate(
-        //     - (textGeometry.boundingBox.max.x - 0.02) * 0.5, // Correct for bevelSize
-        //     - (textGeometry.boundingBox.max.y - 0.02) * 0.5, // Correct for bevelSize
-        //     - (textGeometry.boundingBox.max.z - 0.03) * 0.5  // Correct for bevelThickness
-        // )
-        // console.log(textGeometry.boundingBox)
-
-        /*
-        * Or use this
-        */
         textGeometry.center()
 
         const textMaterial = new THREE.MeshMatcapMaterial( { matcap: matcapTexture} )
         // textMaterial.wireframe = true
         const text = new THREE.Mesh(textGeometry, textMaterial)
         scene.add(text)
-
-
     }
 )
-// const textMesh = new THREE.Mesh(textGeometry, material)
-// scene.add(textMesh)
 
 
-// Cam
-const camera = new THREE.PerspectiveCamera(45, sizes.width/sizes.height)
+/*
+* Camera
+*/ 
+const camera = new THREE.PerspectiveCamera(45, sizes.width/sizes.height, 0.1, 200)
 camera.position.set(0, 0, 5)
-scene.add(camera)
 
-// Renderer
-const renderer = new THREE.WebGLRenderer( {"canvas": canvasEl, antialias: true} )
+/*
+* Renderer
+*/ 
+const renderer = new THREE.WebGLRenderer( {canvas: canvasEl, antialias: true} )
 renderer.setSize(sizes.width, sizes.height)
 
-// Controller
 const controller = new OrbitControls(camera, canvasEl)
 controller.enableDamping = true
 
-// Clock
-const clock = new THREE.Clock()
-
 const tick = () => {
-    const elapsedTime = clock.getElapsedTime()
     controller.update()
-
     renderer.render(scene, camera)
     window.requestAnimationFrame(tick)
 }
